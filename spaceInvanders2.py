02943140e37f733b8149a5afb22130c19f707db5
Piotr Ostrouch <piotr.ostrouch@gmail.com>
2021-02-27 14:33:53 +0100

    font added

--- spaceInvanders2.py
+++ spaceInvanders2.py
@@ -3,6 +3,8 @@ import os
 import time
 import random
 
+pygame.font.init()
+
 
 WIDTH,HEIGHT = 750,650
 
@@ -26,15 +28,29 @@ YELLOW_LASER = pygame.image.load("assets/pixel_laser_yellow.png")
 
 #background
 
-BG  = pygame.image.load("assets/background-black.png")
+BG  = pygame.transform.scale(pygame.image.load("assets/background-black.png"), (WIDTH, HEIGHT))  #skalowanie bg to rozmiaru width i height bo ioryginslny bg jest za maly
 
 def main():
         run = True
         FPS = 60   #frame per second to check collision
         clock = pygame.time.Clock()  #set a clock to chceck events per second myst be on the top of main event loop
+        level = 1
+        lives = 5
+        mainFont = pygame.font.SysFont("comicsans",35)
+
+        def redraw_window():
+            WIN.blit(BG, (0, 0))
+            #pygame.display.update()
+            livesLabel = mainFont.render(f"Lives: {lives}",1,(255,255,255))
+            levelLabel = mainFont.render(f"Level: {level}",1,(255,255,255))
+
+            WIN.blit(livesLabel,(10,10))
+            WIN.blit(levelLabel, (WIDTH- levelLabel.get_width()-10, 10))
+            pygame.display.update()
 
         while run:
             clock.tick(FPS)  #checking how many times per second (int his case 60) we are checking events
+            redraw_window()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
