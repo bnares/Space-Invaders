4f8b0cf5ef48c30e8bcdd40c344c2e9451a8099b
Piotr Ostrouch <piotr.ostrouch@gmail.com>
2021-02-27 13:46:14 +0100

    images loaded

--- spaceInvanders2.py
+++ spaceInvanders2.py
@@ -1,1 +1,25 @@
 import pygame
+import os
+import time
+import random
+
+
+#load images
+RED_SPACE_SHIP = pygame.image.load("assets/pixel_ship_red_small.png")
+GREEN_SPACE_SHIP = pygame.image.load("assets/pixel_ship_green_small.png")
+BLUE_SPACE_SHIP = pygame.image.load("assets/pixel_ship_blue_small.png")
+
+#player images
+YELLOW_SPACE_SHIP = pygame.image.load("assets/pixel_ship_yellow.png")
+
+#lasers
+
+RED_LASER = pygame.image.load("assets/pixel_laser_red.png")
+GREEN_LASER = pygame.image.load("assets/pixel_laser_green.png")
+BLUE_LASER = pygame.image.load("assets/pixel_laser_blue.png")
+YELLOW_LASER = pygame.image.load("assets/pixel_laser_yellow.png")
+
+#background
+
+BG  = pygame.image.load("assets/background-black.png")
+
