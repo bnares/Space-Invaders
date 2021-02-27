61301f6984fcac8bba66dfc4a6f44590346e51e2
Piotr Ostrouch <piotr.ostrouch@gmail.com>
2021-02-27 14:01:49 +0100

    main loop

--- spaceInvanders2.py
+++ spaceInvanders2.py
@@ -4,6 +4,11 @@ import time
 import random
 
 
+WIDTH,HEIGHT = 750,650
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("SPACE SHOOTER")
+
 #load images
 RED_SPACE_SHIP = pygame.image.load("assets/pixel_ship_red_small.png")
 GREEN_SPACE_SHIP = pygame.image.load("assets/pixel_ship_green_small.png")
@@ -22,4 +27,20 @@ YELLOW_LASER = pygame.image.load("assets/pixel_laser_yellow.png")
 #background
 
 BG  = pygame.image.load("assets/background-black.png")
+
+def main():
+        run = True
+        FPS = 60   #frame per second to check collision
+        clock = pygame.time.Clock()  #set a clock to chceck events per second myst be on the top of main event loop
+
+        while run:
+            clock.tick(FPS)  #checking how many times per second (int his case 60) we are checking events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
+
+
+
+main()
 
